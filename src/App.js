4b07531d3332213f48dadcee8bcc8e139a78d262
syukrontanzilah <syukrontanzilah@gmail.com>
2020-09-21 11:47:56 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return(
+          <div className="app">
+          <p>aplikasi tracker</p>
+
+          </div>
+  )
+}
+
+export default App
