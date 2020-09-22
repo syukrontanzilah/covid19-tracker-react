47dc364b43934d07fd9f0381aee7c0a211034900
syukrontanzilah <syukrontanzilah@gmail.com>
2020-09-22 07:40:05 +0700

    get data countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
+import {FormControl, Select, MenuItem, Card, CardContent} from '@material-ui/core'
+
+//https://disease.sh/v3/covid-19/countries
 
 const App = () => {
+const [countries, setCountries] = useState([]);
+
+useEffect(()=>{
+
+const getCountriesData= async () => {
+  await fetch("https://disease.sh/v3/covid-19/countries")
+  .then(response => response.json())
+  .then((data)=> {
+    const countries = data.map(country =>(
+      {
+      name: country.country,
+      value: country.countryInfo.iso2
+      }
+    ));
+setCountries(countries)
+  })
+}
+getCountriesData()
+}, [])
   return(
           <div className="app">
-          <p>aplikasi tracker</p>
+
+          <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+              <FormControl className="app__dropdown">
+                <Select
+                variant="outlined"
+                value="abc"
+                onChange
+                >
+                {
+                  countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                  ))
+                }
+
+                </Select> 
+
+              </FormControl>
+          </div>
+
+      
 
           </div>
   )
